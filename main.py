79e1512136c56655d755e2cb0bae64a3a1f3eb7d
Sussy_Hyena <kevin.currier5@gmail.com>
2026-06-13 09:54:28 -0600

    Setup cont, add README.md

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import os
 
 def main():
+    
+    print("============ SORT BY EXTENSION ============")
+    source_folder = input("Source folder (enter full path of folder where you're moving files FROM): ")
+    dest_folder = input("Destination folder (enter full path of folder where you want to move files TO: ")
+    
