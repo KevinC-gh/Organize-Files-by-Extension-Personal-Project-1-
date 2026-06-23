95526c0a1e9202d063780cfd5fdaff92b6463e45
Sussy_Hyena <kevin.currier5@gmail.com>
2026-06-22 20:15:08 -0600

    I: Added entry box to GUI to allow multiple file types to be selected

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #run: python3 main.py
+#cd workspace/bootdev/personal_project_1
 import os
 import tkinter as tk
 from tkinter import ttk
@@ -20,14 +21,18 @@ def main():
     dest_folder_button = ttk.Button(root, text="Add Destination Folder", command=lambda: add_dest_folder(dest_folder_entry))
     dest_folder_button.grid(row=1, column=0, padx=20, pady=5, sticky="w")
 
+    dropdown_entry = ttk.Entry(root, width=50)
+    dropdown_entry.grid(row=2, column=1, padx=20, pady=5, sticky="w")
+
     options = [".png", ".jpg", ".docx", ".pdf", ".txt"]
-    dropdown = ttk.Combobox(root, width=15, values=options)
+    dropdown = ttk.Combobox(root, width=16, values=options, state="readonly")
     dropdown.grid(row=2, column=0, padx=20, pady=5, sticky="w")
-    dropdown.insert(0, "(Select File Type)")
-    
+    dropdown.set("Select File Type(s)")
+    dropdown.bind("<<ComboboxSelected>>", lambda event: list_selected(event, dropdown, dropdown_entry))
+
     run_button = ttk.Button(root, text="Run", command=lambda: run(dropdown, source_folder_entry, dest_folder_entry))
     run_button.grid(row=3, column=0, padx=20, pady=10, sticky="w")
-    
+
     root.mainloop()
 
 def add_source_folder(source_folder_entry):
@@ -37,6 +42,11 @@ def add_source_folder(source_folder_entry):
 def add_dest_folder(dest_folder_entry):
     folder_path = filedialog.askdirectory(initialdir="/mnt/c/users", title="Select Folder")
     dest_folder_entry.insert(0, folder_path)
+
+def list_selected(_, dropdown, dropdown_entry):
+    dropdown_selection = dropdown.get()
+    dropdown_entry.insert(tk.END, dropdown_selection)
+    dropdown.set("Select File Type(s)")
 
 def run(dropdown, source_folder_entry, dest_folder_entry):
     file_type_selection = dropdown.get()
@@ -50,5 +60,5 @@ def run(dropdown, source_folder_entry, dest_folder_entry):
             full_dest_path = os.path.abspath(os.path.join(dest_folder, file))
             os.rename(full_source_path, full_dest_path)
             print(f"{file} moved successfully")
-            
+
 main()
